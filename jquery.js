62f4ffc685d40a4aae6f30a029d444c114fb85bc
SaStir <germansorokin58@gmail.com>
2024-09-29 14:06:52 +0300

    d

--- jquery.js
+++ jquery.js
@@ -1,11 +1,11 @@
 // Show dropdown when clicked
 $('#header-btn').on('click', function (e) {
-    $('#MENU_ID_HERE').toggleClass('active');
+    $('header-btn').toggleClass('active');
     $('.nav-btn').toggleClass('active');
 });
 
 // Hide menu after clicking menu item
 $('.dropdown-menu li').on('click', function (e) {
-    $('#MENU_ID_HERE').removeClass('active');
+    $('#header-menu').removeClass('active');
     $('.nav-btn').removeClass('active');
 });
